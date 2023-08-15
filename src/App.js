863467f703b2dace14c849521e2e2a5b35cfdbbe
Adeed Mahar <32008987+Adeed336@users.noreply.github.com>
2023-08-15 23:08:11 +0500

    updates 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p1 className="Rotate-text">
-          *
-        </p1>
-        <p2>
-          *
-        </p2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          project
-        </a>
-        <a className="App-link" href="/other-page">HOME</a>
+        <p>Only thing certain in life is death</p>
       </header>
     </div>
   );
